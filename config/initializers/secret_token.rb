f0a8cecb7c07ca79ff6b1d35d3aaa7a55f39481f
Opuncjusz <Aron@.(none)>
2012-10-12 22:10:33 +0200

    start

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sklep::Application.config.secret_token = 'a9d0028220d685cb6bb1e4e046c528a4adc019ad1d065e0eae20b076d02655dc51dc2b9243481114b3a2661cdba82b63384cd8754132cc5c341967965200434f'
